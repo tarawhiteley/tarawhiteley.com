17e32f2181b08c476be1f6da59b8a7094073af99
Tara Whiteley <tarawhiteley@gmail.com>
2017-09-02 23:30:45 -0400

    completes all major content work

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,3 +1,19 @@
+$(document).ready(function() {
+$(".fancybox").fancybox();
+});
+
+$("#js-rotating").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "bounceIn",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 2000,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+});
+
 //Animsition
 $(".animsition").animsition({
   inClass: 'fade-in-up',
@@ -21,7 +37,3 @@ $(".animsition").animsition({
   overlayParentElement : 'body',
   transition: function(url){ window.location.href = url; }
 });
-
-	$(document).ready(function() {
-	$(".fancybox").fancybox();
-	});
